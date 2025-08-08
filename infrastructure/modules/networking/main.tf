466823489e249e2e18f50b751cad688a7f4367a0
CuciureanuDan <dancuciureanu4@yahoo.com>
2025-08-08 12:21:11 +0300

    add firewall rules for docker swarm

--- infrastructure/modules/networking/main.tf
+++ infrastructure/modules/networking/main.tf
@@ -15,7 +15,7 @@ resource "google_compute_subnetwork" "public-subnet" {
 resource "google_compute_firewall" "allow_ssh" {
     name = "${var.vpc_name}-allow-ssh"
     network = google_compute_network.vpc.name
-    description = "Create firewall rules"
+    description = "Allow SSH and ICMP"
 
     allow {
         protocol = "icmp"
@@ -26,4 +26,25 @@ resource "google_compute_firewall" "allow_ssh" {
       ports = ["22"]
     }
     source_ranges = ["0.0.0.0/0"] 
+}
+
+resource "google_compute_firewall" "allow_docker_swarm" {
+    name = "${var.vpc_name}-allow-docker-swarm"
+    network = google_compute_network.vpc.name
+    description = "Allow Docker Swarm communication"
+
+    allow {
+        protocol = "tcp"
+        ports = ["2377", "7946"]
+    }
+
+    allow {
+        protocol = "udp"
+        ports = ["7946", "4789"]
+    }
+
+    # target_tags = []    # for future improvement   
+    #
+    source_ranges = ["10.0.0.0/24"] #  ["172.16.0.0/12", "192.168.0.0/16"]
+
 }
