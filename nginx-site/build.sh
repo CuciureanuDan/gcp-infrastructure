15b3dbbbee3d244ddf81e90db4f77cceb3ba08d5
CuciureanuDan <dancuciureanu4@yahoo.com>
2025-08-12 10:00:26 +0300

    add the image to docker hub

--- nginx-site/build.sh
+++ nginx-site/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker build -t test-site:latest .
+DOCKER_USER=dancu25
+IMAGE_NAME=test-site
+TAG=latest
+
+docker build -t $DOCKER_USER/$IMAGE_NAME:$TAG .
+docker push $DOCKER_USER/$IMAGE_NAME:$TAG
